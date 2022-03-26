2c350c025778ff5cd8c960cfe99a881c8e0d21be
Yones Montja <yonesmontja@gmail.com>
2022-03-27 08:26:33 +0900

    menambahakan PDF, Session, User dll

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use App\Exports\SiswaExport;
-use App\Imports\SiswaImport; 
-use App\Imports\UserImport;
-use Maatwebsite\Excel\Facades\Excel;
-use App\Models\Siswa;
-use App\Models\User;
-use App\Models\Penilaian;
 use PDF;
 use Session;
+use App\Models\User;
+use App\Models\Siswa;
+use App\Models\Penilaian;
+use App\Imports\UserImport;
+use Illuminate\Support\Str;
+use App\Exports\SiswaExport;
+use App\Imports\SiswaImport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use Intervention\Image\Facades\Image;
 
+use Illuminate\Support\Facades\Input;
 class SiswaController extends Controller
 {
     public function index()
@@ -31,16 +33,26 @@ class SiswaController extends Controller
 
     public function edit(Siswa $siswa)
     {
-        return view('siswa/edit',['siswa'=>$siswa]); 
+        return view('siswa/edit',['siswa'=>$siswa]);
     }
     public function update(Request $request, Siswa $siswa)
     {
 
         $siswa ->update($request->all());
-        if($request->hasFile('avatar')){
-            $request->file('avatar')->move('images/',$request->file('avatar')->getClientOriginalName());
-            $siswa->avatar= $request->file('avatar')->getClientOriginalName();
-            $siswa->save();
+        //if($request->hasFile('avatar')){
+        //    $request->file('avatar')->move('images/',$request->file('avatar')->getClientOriginalName());
+        //    $siswa->avatar= $request->file('avatar')->getClientOriginalName();
+        //    $siswa->save();
+        //}
+        if ($request->hasFile('avatar')) {
+            $siswa->delete_avatar();
+            $avatar = $request->file('avatar');
+            $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+            $img = Image::make($avatar->path());
+            $img->resize('120', '120')
+                ->save(public_path('/images') . '/small_' . $file_name);
+            $avatar->move('/images', $file_name);
+            $siswa->avatar = $file_name;
         }
         return redirect('/siswa')->with('sukses','berhasil diupdate!');
     }
@@ -61,25 +73,52 @@ class SiswaController extends Controller
             'agama' => 'required',
             'avatar' => 'mimes:jpeg,jpg,png',
         ]);
+        $avatar = $request->file('avatar');
+        $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+        $img = Image::make($avatar->path());
+        $img->resize('120', '120')
+            ->save(public_path('/images') . '/small_' . $file_name);
+
+        $avatar->move('/images', $file_name);
 
         //insert ke tabel Users
-        $user = new \App\Models\User;
+        $user = new User();
         $user -> role = 'siswa';
         $user -> name = $request -> nama_depan;
         $user -> email = $request -> email;
         $user -> password = bcrypt('rahasia');
         $user -> remember_token = Str::random(60);
+        $user -> avatar = $file_name;
         $user -> save();
         //insert ke tabel Siswa
-        $request -> request -> add(['user_id' => $user -> id]);
-        $siswa = \App\Models\Siswa::create($request -> all());
+        $input = $request->all();
+        $user_id = $user->id;
+        //dd($user_id);
+        $siswa = new Siswa();
+        //dd($siswa);
+        $siswa -> user_id = $user_id;
+        $siswa -> nama_depan = $input['nama_depan'];
+        $siswa -> nama_belakang = $input['nama_belakang'];
+        $siswa -> email = $input['email'];
+        $siswa -> nis = $input['nis'];
+        $siswa -> nisn = $input['nisn'];
+        $siswa -> jenis_kelamin = $input['jenis_kelamin'];
+        $siswa -> agama = $input['agama'];
+        $siswa -> alamat = $input['alamat'];
+        $siswa -> avatar = $file_name;
+        //$request -> request -> add(['user_id' => $user -> id]);
+        //dd($request -> request -> add(['user_id' => $user -> id]));
+        //$siswa = Siswa::create($request -> all());
         $siswa -> password = bcrypt('rahasia');
-        $siswa -> role = 'siswa';
-        if($request->hasFile('avatar')){
-            $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
-            $siswa->avatar= $request->file('avatar')->getClientOriginalName();
-            $siswa->save();
-        }
+        //dd($siswa);
+
+        //if($request->hasFile('avatar')){
+        //    $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
+        //    $siswa->avatar= $request->file('avatar')->getClientOriginalName();
+        //    $siswa->save();
+        //}
+
+        $siswa -> save();
         //return $request -> all();
         return redirect('/test')->with('sukses','berhasil diinput');
     }
@@ -87,7 +126,7 @@ class SiswaController extends Controller
     public function testaktivasi(Request $request,$id)
     {
         $siswa = Siswa::find($id);
-        
+
         $user = new \App\Models\User;
         //dd($siswa);
         $user -> role = 'siswa';
@@ -100,30 +139,48 @@ class SiswaController extends Controller
         $status = '1';
         $request -> request -> add(['user_id' => $user -> id,'status' => $status]);
         $siswa ->update($request->all());
-        
+
         //dd($siswa);
 
-        return redirect('/test')->with('sukses', 'berhasil diaktivasi'); 
+        return redirect('/test')->with('sukses', 'berhasil diaktivasi');
     }
 
     public function testedit(Siswa $siswa)
     {
-        return view('siswa/testedit',['siswa'=>$siswa]); 
+        return view('siswa/testedit',['siswa'=>$siswa]);
     }
     public function testupdate(Request $request, Siswa $siswa)
     {
 
         $siswa ->update($request->all());
-        if($request->hasFile('avatar')){
-            $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
-            $siswa->avatar= $request->file('avatar')->getClientOriginalName();
-            $siswa->save();
+        //if($request->hasFile('avatar')){
+        //    $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
+        //    $siswa->avatar= $request->file('avatar')->getClientOriginalName();
+        //    $siswa->save();
+        //}
+        if ($request->hasFile('avatar')) {
+            $siswa->delete_avatar();
+            $avatar = $request->file('avatar');
+            $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+            $img = Image::make($avatar->path());
+            $img->resize('120', '120')
+                ->save(public_path('/images') . '/small_' . $file_name);
+            $avatar->move('/images', $file_name);
+            $siswa->avatar = $file_name;
         }
+        $siswa->nama_depan = $request->nama_depan;
+        $siswa->nama_belakang = $request->nama_belakang;
+        $siswa->email = $request->email;
+        $siswa->jenis_kelamin = $request->jenis_kelamin;
+        $siswa->agama = $request->agama;
+        $siswa->alamat = $request->alamat;
+        $siswa->save();
         return redirect('/test')->with('sukses','berhasil diupdate!');
     }
-    public function testdelete(Siswa $siswa)
+    public function testdelete(Siswa $siswa, $user_id)
     {
-        $siswa ->delete();
+        $siswa -> user() -> detach($user_id);
+        //$user -> delete();
         return redirect('/test')->with('sukses','berhasil dihapus!');
     }
     public function testprofile($id)
@@ -138,17 +195,17 @@ class SiswaController extends Controller
         // data untuk Chart.js
         $categories = [];
         $data = [];
-        
+
         foreach($matapelajaran as $mp){
             if($siswa->mapel()->wherePivot('mapel_id',$mp->id)->first()){
                 $categories[] = $mp ->nama_mapel;
-                
+
                 $data[] = $siswa -> mapel() -> wherePivot('mapel_id',$mp ->id) -> first()->pivot->nilai;
             }
         }
         $matpel = collect($categories);
         $average = collect($data)->avg();
-        
+
         //dd($matpel);
         //dd($average);
         //dd(json_encode($categories));
@@ -189,17 +246,17 @@ class SiswaController extends Controller
         $this->validate($request, [
             'file' => 'required|mimes:csv,xls,xlsx'
         ]);
- 
+
         // menangkap file excel
         $file = $request->file('file');
-        
+
         // membuat nama file unik
         $nama_file = rand().$file->getClientOriginalName();
- 
+
         // upload ke folder file_siswa di dalam folder public
         $file->move('file_siswa',$nama_file);
 
- 
+
         // import data
         Excel::import(new SiswaImport, public_path('/file_siswa/'.$nama_file));
         //Excel::import(new UserImport, public_path('/file_siswa/'.$nama_file));
@@ -210,10 +267,10 @@ class SiswaController extends Controller
             $siswa->avatar= $request->file('avatar')->getClientOriginalName();
             $siswa->save();
         }
- 
+
         // notifikasi dengan session
         Session::flash('sukses','Berhasil Diimport!');
- 
+
         // alihkan halaman kembali
         return redirect('/test');
     }
