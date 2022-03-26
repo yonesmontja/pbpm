c86e341801df92751fbda06a9d9db65d1c88b55f
Yones Montja <yonesmontja@gmail.com>
2022-03-26 16:19:29 +0900

    use Intervention\Image\Facades\Image;

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-
 use App\Models\User;
 use App\Models\Siswa;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class UserController extends Controller
 {
@@ -27,7 +28,7 @@ class UserController extends Controller
     public function my_profile($id)
     {
         $user = \App\Models\User::find($id);
-        
+
         return view('profile.my_profile',['user' => $user]);
     }
 
@@ -69,20 +70,27 @@ class UserController extends Controller
             'email' => 'required|email|unique:users',
             'avatar' => 'mimes:jpeg,jpg,png',
         ]);
+        $avatar = $request->file('avatar');
+        $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+        $img = Image::make($avatar->path());
+        $img->resize('180', '120')
+            ->save(public_path('users/posting') . '/small_' . $file_name);
 
+        $avatar->move('users/posting', $file_name);
         //insert ke tabel Users
-        $user = new \App\Models\User;
+        $user = new User();
         $user -> role = $request -> role;
         $user -> name = $request -> name;
         $user -> email = $request -> email;
+        $user -> avatar = $file_name;
         $user -> password = bcrypt('rahasia');
         $user -> remember_token = Str::random(60);
-                if($request->hasFile('avatar')){
-            $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
-            $user->avatar= $request->file('avatar')->getClientOriginalName();
-            $user->save();
-        }
-        //$user -> save();
+        //        if($request->hasFile('avatar')){
+        //    $request->file('avatar')->move('/storage/images',$request->file('avatar')->getClientOriginalName());
+        //    $user->avatar= $request->file('avatar')->getClientOriginalName();
+        //    $user->save();
+        //}
+        $user -> save();
 
         //return $request -> all();
 
@@ -94,13 +102,23 @@ class UserController extends Controller
     }
     public function userupdate(Request $request, User $user)
     {
-
-        $user ->update($request->all());
-        if($request->hasFile('avatar')){
-            $request->file('avatar')->move('images/',$request->file('avatar')->getClientOriginalName());
-            $user->avatar= $request->file('avatar')->getClientOriginalName();
-            $user->save();
+        $request->validate([
+            'name' => 'required',
+        ]);
+        //$user ->update($request->all());
+        if ($request->hasFile('avatar')) {
+            $user->delete_avatar();
+            $avatar = $request->file('avatar');
+            $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+            $img = Image::make($avatar->path());
+            $img->resize('120', '120')
+                ->save(public_path('users/posting') . '/small_' . $file_name);
+            $avatar->move('users/posting', $file_name);
+            $user->avatar = $file_name;
         }
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
         return redirect('/user')->with('sukses','berhasil diupdate!');
     }
 }
