0956c8703d0d188de7950bcb171b4d36c02476f3
Yones Montja <yonesmontja@gmail.com>
2022-03-28 16:47:53 +0900

    import Image

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use App\Models\Guru;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class GuruController extends Controller
 {
@@ -32,9 +33,15 @@ class GuruController extends Controller
             'alamat' => 'required',
             'avatar' => 'mimes:jpeg,jpg,png',
         ]);
+        $avatar = $request->file('avatar');
+        $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+        $img = Image::make($avatar->path());
+        $img->resize('120', '120')
+            ->save(public_path('/images') . '/small_' . $file_name);
 
+        $avatar->move('/images', $file_name);
         //insert ke tabel Users
-        $user = new \App\Models\User;
+        $user = new User();
         $user -> role = 'guru';
         $user -> name = $request -> nama_guru;
         $user -> email = $request -> email;
@@ -42,14 +49,73 @@ class GuruController extends Controller
         $user -> remember_token = Str::random(60);
         $user -> save();
         //insert ke tabel Guru
-        $request -> request -> add(['user_id' => $user -> id]);
-        $guru = \App\Models\Guru::create($request -> all());
-        if($request->hasFile('avatar')){
-            $request->file('avatar')->move('images/',$request->file('avatar')->getClientOriginalName());
-            $guru->avatar= $request->file('avatar')->getClientOriginalName();
-            $guru->save();
-        }
+        $input = $request->all();
+        $user_id = $user->id;
+        $guru = new Guru();
+        //$request -> request -> add(['user_id' => $user -> id]);
+        $guru -> user_id = $user_id;
+        $guru -> nama_guru = $input['nama_guru'];
+        $guru -> telpon = $input['telpon'];
+        $guru -> alamat = $input['alamat'];
+        $guru -> kode_guru = $input['kode_guru'];
+        $guru -> jk = $input['jk'];
+        $guru -> is_bk = $input['is_bk'];
+        $guru -> stat_data = $input['stat_data'];
+        $guru -> status = $input['status'];
+        $guru -> email = $input['email'];
+        $guru -> avatar = $file_name;
+        $guru->save();
         //return $request -> all();
-        return redirect('/guru')->with('sukses','berhasil diinput');    	
+        return redirect('/guru')->with('sukses','berhasil diinput');
+    }
+    public function guruedit(Guru $guru)
+    {
+        return view('guru/guruedit',['guru'=>$guru]);
+    }
+    public function guruupdate(Request $request, Guru $guru)
+    {
+
+        $guru ->update($request->all());
+        //if($request->hasFile('avatar')){
+        //    $request->file('avatar')->move('/images/',$request->file('avatar')->getClientOriginalName());
+        //    $guru->avatar= $request->file('avatar')->getClientOriginalName();
+        //    $guru->save();
+        //}
+        if ($request->hasFile('avatar')) {
+            $guru->delete_avatar();
+            $avatar = $request->file('avatar');
+            $file_name = rand(1000, 9999) . $avatar->getClientOriginalName();
+            $img = Image::make($avatar->path());
+            $img->resize('120', '120')
+                ->save(public_path('/images') . '/small_' . $file_name);
+            $avatar->move('/images', $file_name);
+            $guru->avatar = $file_name;
+        }
+        //$user = new User();
+        //insert ke tabel Guru
+        $input = $request->all();
+        //$user_id = $user->id;
+        //$guru = new Guru();
+        //$request -> request -> add(['user_id' => $user -> id]);
+        $guru -> user_id = $input['user_id'];
+        $guru -> nama_guru = $input['nama_guru'];
+        $guru -> telpon = $input['telpon'];
+        $guru -> alamat = $input['alamat'];
+        $guru -> kode_guru = $input['kode_guru'];
+        $guru -> jk = $input['jk'];
+        $guru -> is_bk = $input['is_bk'];
+        $guru -> stat_data = $input['stat_data'];
+        $guru -> status = $input['status'];
+        $guru -> email = $input['email'];
+        $guru -> avatar = $file_name;
+
+        $guru->save();
+        return redirect('/guru')->with('sukses','berhasil diupdate!');
+    }
+    public function gurudelete(Guru $guru)
+    {
+        $guru ->delete_avatar();
+        $guru ->delete();
+        return redirect('/guru')->with('sukses','berhasil dihapus!');
     }
 }
