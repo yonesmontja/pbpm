880a89a203f8d80ee0d3413a2a2f9e66d98ed43d
Yones Montja <yonesmontja@gmail.com>
2022-03-31 10:00:38 +0900

    buat tabel database jadwals

---
+++ database/migrations/2022_03_29_120309_create_jadwals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJadwalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('allDay');
+            $table->string('color');
+            $table->string('textColor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jadwals');
+    }
+}
