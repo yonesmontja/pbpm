561020e5fef9fbbbaba32d3a83552bdcc86134f1
Yones Montja <yonesmontja@gmail.com>
2022-04-27 14:09:36 +0900

    buat nilai controller

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Guru;
+use App\Models\Mapel;
 use App\Models\Nilai;
+use App\Models\Siswa;
+use App\Models\Penilaian;
+use Illuminate\Http\Request;
+use App\Models\Kompetensiinti;
 
 class NilaiController extends Controller
 {
     //
     public function nilai()
     {
-    	$data_nilai = \App\Models\Nilai::all();
-    	return view('nilai.index',['data_nilai' => $data_nilai]);
+    	$data_nilai = Nilai::all();
+        $kompetensiinti = Kompetensiinti::all();
+        $mapel = Mapel::all();
+        $siswa = Siswa::all();
+        $penilaian = Penilaian::all();
+        $guru = Guru::all();
+
+
+        //dd($guru);
+    	return view('nilai.index',['penilaian' => $penilaian,'siswa' => $siswa,'mapel' => $mapel,'kompetensiinti' => $kompetensiinti,'data_nilai' => $data_nilai,'guru' => $guru]);
     }
     public function nilaicreate(Request $request)
     {
@@ -27,7 +40,7 @@ class NilaiController extends Controller
     }
     public function nilaiedit(Nilai $nilai)
     {
-        return view('nilai.nilaiedit',['nilai'=>$nilai]); 
+        return view('nilai.nilaiedit',['nilai'=>$nilai]);
     }
     public function nilaiupdate(Request $request, Nilai $nilai)
     {
