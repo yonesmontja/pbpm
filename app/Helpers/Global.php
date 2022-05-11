b88b778e1bd279ae5f1fe356f99a72cd8f733554
Yones Montja <yonesmontja@gmail.com>
2022-05-11 17:19:05 +0900

    buat fungsi set_activeuntuk aktifasi link menu sesuai url nya

--- app/Helpers/Global.php
+++ app/Helpers/Global.php
@@ -338,3 +338,17 @@ function terbilang($x, $style=4){
     }
     return $hasil;
  }
+function set_active($uri, $output = "active")
+{
+ if( is_array($uri) ) {
+   foreach ($uri as $u) {
+     if (Route::is($u)) {
+       return $output;
+     }
+   }
+ } else {
+   if (Route::is($uri)){
+     return $output;
+   }
+ }
+}
