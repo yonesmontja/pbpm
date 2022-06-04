3723e77b5c410d9109e501e91516741da2b2f5ac
yonesmontja <yonesmontja@gmail.com>
2022-06-04 20:41:36 +0900

    user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,8 +25,8 @@ class UserFactory extends Factory
         return [
             'role' => 'admin',
             'avatar' => 'default.jpg',
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => 'admin',
+            'email' => 'admin.admin@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
