b56dc68558356bb1d58dddb6943cf5230b18d19b
Yones Montja <yonesmontja@gmail.com>
2023-05-21 17:08:51 +0900

    ubah kurikulum.index.blade.php

--- resources/views/kurikulum/index.blade.php
+++ resources/views/kurikulum/index.blade.php
@@ -79,7 +79,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/1/profile">Kelas 1</a></span>
                                 <span class="info-box-number">
@@ -95,7 +94,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/2/profile">Kelas 2</a></span>
                                 <span class="info-box-number">
@@ -111,7 +109,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/3/profile">Kelas 3</a></span>
                                 <span class="info-box-number">
@@ -127,7 +124,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/4/profile">Kelas 4</a></span>
                                 <span class="info-box-number">
@@ -143,7 +139,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/5/profile">Kelas 5</a></span>
                                 <span class="info-box-number">
@@ -159,7 +154,6 @@
                     <div class="col-12 col-sm-6 col-md-2">
                         <div class="info-box">
                             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
-
                             <div class="info-box-content">
                                 <span class="info-box-text"><a href="/kelas/6/profile">Kelas 6</a></span>
                                 <span class="info-box-number">
@@ -172,16 +166,13 @@
                         <!-- /.info-box -->
                     </div>
                     <!-- /.col -->
-
                 </div>
                 <!-- /.row -->
-
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-header">
                                 <h5 class="card-title">Laporan Bulanan</h5>
-
                                 <div class="card-tools">
                                     <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                         <i class="fas fa-minus"></i>
