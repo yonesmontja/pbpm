f0b4c6bb04c0d47bbad8adba902f9618fe803543
Yones Montja <yonesmontja@gmail.com>
2023-06-06 19:54:03 +0700

    lupa lagi satu yang belum direvisi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1979,7 +1979,7 @@ class DashboardController extends Controller
             'guru_rombel' => $guru_rombel,
             'kelas_rombel' => $kelas_rombel,
             //'jumlah_penilaian' => $jumlah_penilaian,
-            'kelas_sub' => $kelas_sub,
+            //'kelas_sub' => $kelas_sub,
             'nilai_start' => $nilai_start,
             'nilai_end' => $nilai_end,
             'tahunpel' => $tahunpel,
