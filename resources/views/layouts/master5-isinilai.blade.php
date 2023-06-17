ed81d4f02179876b35823b9fe4ea6f0c1a400d23
Yones Montja <yonesmontja@gmail.com>
2023-06-17 19:22:42 +0700

    uji lagi

--- resources/views/layouts/master5-isinilai.blade.php
+++ resources/views/layouts/master5-isinilai.blade.php
@@ -163,9 +163,9 @@
     <!-- Bootstrap4 Duallistbox -->
     {{-- <script src="{{ asset('/admin/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js') }}"></script> --}}
     <!-- InputMask -->
-    {{-- <script src="{{ asset('/admin/plugins/moment/moment.min.js') }}"></script>
+    <script src="{{ asset('/admin/plugins/moment/moment.min.js') }}"></script>
     <script src="{{ asset('/admin/plugins/inputmask/min/jquery.inputmask.bundle.min.js') }}"></script>
-    <script src="{{ asset('/admin/plugins/daterangepicker/daterangepicker.js') }}"></script> --}}
+    <script src="{{ asset('/admin/plugins/daterangepicker/daterangepicker.js') }}"></script>
     <!-- bootstrap color picker -->
     {{-- <script src="{{ asset('/admin/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js') }}"></script> --}}
     <!-- Tempusdominus Bootstrap 4 -->
