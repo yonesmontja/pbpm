fa7bd3ac719273cd0cc2f14e51df734c66796477
Yones Montja <yonesmontja@gmail.com>
2023-06-17 09:44:54 +0700

    link jquery

--- resources/views/layouts/master5-isinilai.blade.php
+++ resources/views/layouts/master5-isinilai.blade.php
@@ -62,7 +62,7 @@
         @include('layouts.includes._footer')
     </div>
     <!-- ./wrapper -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('/admin/plugins/Jquery-Table-Check-All/dist/TableCheckAll.js') }}"></script>
